3cc8b8a466cfd49db91625b1728951fd0c73efa2
Anthony Blackwell <anthonyb@goop.com>
2020-08-26 06:39:27 -0700

    Uncommented snippet - 2

--- dist/min.template.js
+++ dist/min.template.js
@@ -1,6 +1,6 @@
 module.exports=function(settings) {
 var __t, __p = '';
-__p += '!function(){rudderanalytics=window.rudderanalytics=[];for(var a=["load","page","track","identify","alias","group","ready","reset","getAnonymousId","setAnonymousId"],n=0;n<a.length;n++){var t=a[n];rudderanalytics[t]=function(a){return function(){rudderanalytics.push([a].concat(Array.prototype.slice.call(arguments)))}}(t)}\n' +
+__p += '!function(){rudderanalytics=window.rudderanalytics=[];for(var t=["load","page","track","identify","alias","group","ready","reset","getAnonymousId","setAnonymousId"],a=0;a<t.length;a++){var e=t[a];rudderanalytics[e]=function(t){return function(){rudderanalytics.push([t].concat(Array.prototype.slice.call(arguments)))}}(e)}rudderanalytics.load=function(t){var a=document.createElement("script");a.type="text/javascript";a.async=!0;a.src="https://cdn.rudderlabs.com/v1/rudder-analytics.min.js";var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(a,e);analytics._loadOptions=t};\n' +
 ((__t = ( settings.load )) == null ? '' : __t) +
 '\n' +
 ((__t = ( settings.page )) == null ? '' : __t) +
